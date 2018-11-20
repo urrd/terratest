89041126df1478e2958162942161873e9ea580a8
Martin S. Andersen <maa@universal-robots.com>
2018-11-20 11:14:46 +0100

    Added target to options for plan, apply and destroy

--- modules/terraform/options.go
+++ modules/terraform/options.go
@@ -18,4 +18,5 @@ type Options struct {
 	Upgrade                  bool                   // Whether the -upgrade flag of the terraform init command should be set to true or not
 	NoColor                  bool                   // Whether the -no-color flag will be set for any Terraform command or not
 	SshAgent                 *ssh.SshAgent          // Overrides local SSH agent with the given in-process agent
+	Targets                  map[string]string      // Specifies -target= arguments for terraform plan, apply and destroy
 }
